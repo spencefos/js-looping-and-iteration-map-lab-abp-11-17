8a83d5029a6d1b4d2adc4e4977b46159a51840d0
Spencer Foster <fosterjs@me.com>
2017-11-19 16:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 function lowerCaseDrivers(drivers) {
    return drivers.map(driver => driver.toLowerCase());
  }
- 
+
  function nameToAttributes(drivers) {
    return drivers.map(driver => ({ firstName: driver.split(' ')[0], lastName: driver.split(' ')[1] }));
  }
- 
+
  function attributesToPhrase(drivers) {
    return drivers.map(driver => `${driver.name} is from ${driver.hometown}`);
  }
